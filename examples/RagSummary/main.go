42ba5c97013e5dbc53c04889cfa7aece88a1203b
Frederick Mannings <fred.mannings@solvicode.com>
2025-01-19 14:56:40 +0000

    Added an example for RAG

---
+++ examples/RagSummary/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	dr "github.com/Predixus/DynaRAG"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	client, err := dr.New(dr.Config{
+		PostgresConnStr: "postgresql://admin:root@localhost:5053/main?sslmode=disable",
+		LLMProvider:     args[0],
+		LLMToken:        args[1],
+	})
+	if err != nil {
+		slog.Error("Unable to initialise the DynaRAG client", "message", err)
+		return
+	}
+
+	err = client.Chunk(context.Background(), "London is the capital of england", "./test", nil)
+	if err != nil {
+		slog.Error("Failed to chunk text", "message", err)
+		return
+	}
+
+	writer := os.Stdout
+	nDocs := int8(1)
+	err = client.Query(
+		context.Background(),
+		"What is the capital of England?",
+		&nDocs,
+		nil,
+		writer,
+	)
+	if err != nil {
+		slog.Error("Failed to execute query", "message", err)
+		return
+	}
+}
